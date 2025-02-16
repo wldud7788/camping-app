e2f0b3cd6c9b5e526e7416012d244e059939c3a0
wldud7788 <rooftop7788@naver.com>
2025-02-17 03:32:16 +0900

    fix: 임포트 수정

--- src/pages/CampingAll.jsx
+++ src/pages/CampingAll.jsx
@@ -1,4 +1,4 @@
-import { CampingList } from "../components/campingAll/CampingList";
+import { CampingList } from "@/components/campingAll/CampingList";
 
 export const CampingAll = () => {
   return (
