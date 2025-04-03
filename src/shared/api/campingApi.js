4e45e26b7c60fc35fbbd1e9a48aa4476d58fe9e3
wldud7788 <rooftop7788@naver.com>
2025-04-03 18:14:29 +0900

    검색 결과 무한 스크롤 추가

--- src/shared/api/campingApi.js
+++ src/shared/api/campingApi.js
@@ -8,6 +8,7 @@ export const CAMPING_API = {
   _type: "json",
   ROWS_PER_PAGE: 40, // 병렬 처리 시 1회 요청 당 불러올 행 수
   TOTAL_COUNT: 4109, // 전체 데이터 조회 시 사용할 행 수
+  ITEMS_PER_PAGE: 6,
 };
 
 export const campingAPi = {
@@ -28,7 +29,7 @@ export const campingAPi = {
   searchList: async (
     keyword,
     pageNo = 1,
-    numOfRows = CAMPING_API.TOTAL_COUNT
+    numOfRows = CAMPING_API.ITEMS_PER_PAGE
   ) => {
     try {
       const encodedKeyword = encodeURIComponent(keyword);
